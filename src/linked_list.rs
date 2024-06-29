6d993f6d446765fae410c32567224c8fa826b6d3
Taishi Naka <taishi.develop@gmail.com>
2024-06-29 23:13:22 +0900

    implement prepend for LinkedList

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -24,6 +24,25 @@ impl<T: std::fmt::Debug> LinkedList<T> {
 
         self.head = Some(new_node);
     }
+
+    pub fn append(&mut self, value: T) {
+        let new_node = Rc::new(RefCell::new(Node { value, next: None }));
+        match self.head.clone() {
+            None => self.head = Some(new_node),
+            Some(head) => {
+                let mut current = head;
+                while let Some(next_node) = {
+                    // ブロックを作り、nextという一時変数をつくって返すことで
+                    // メインのwhileブロックの中でcurrentを扱えるようにする
+                    let next = current.borrow().next.clone();
+                    next
+                } {
+                    current = next_node;
+                }
+                current.borrow_mut().next = Some(new_node);
+            }
+        }
+    }
 }
 
 impl<T: std::fmt::Debug> Display for LinkedList<T> {
@@ -51,17 +70,60 @@ mod tests {
     }
 
     #[test]
-    fn prepend_1() {
+    fn prepend_once() {
         let mut list = LinkedList::new();
         list.prepend(1);
         assert_eq!(format!("{}", list), "1 -> None");
     }
 
     #[test]
-    fn prepend_2() {
+    fn prepend_twice() {
         let mut list = LinkedList::new();
         list.prepend(1);
         list.prepend(2);
         assert_eq!(format!("{}", list), "2 -> 1 -> None");
+    }
+
+    #[test]
+    fn prepend_thrice() {
+        let mut list = LinkedList::new();
+        list.prepend(1);
+        list.prepend(2);
+        list.prepend(3);
+        assert_eq!(format!("{}", list), "3 -> 2 -> 1 -> None");
+    }
+
+    #[test]
+    fn append_when_list_is_empty() {
+        let mut list = LinkedList::new();
+        list.append(1);
+        assert_eq!(format!("{}", list), "1 -> None");
+    }
+
+    #[test]
+    fn append_twice() {
+        let mut list = LinkedList::new();
+        list.append(1);
+        list.append(2);
+        assert_eq!(format!("{}", list), "1 -> 2 -> None");
+    }
+
+    #[test]
+    fn append_thrice() {
+        let mut list = LinkedList::new();
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        assert_eq!(format!("{}", list), "1 -> 2 -> 3 -> None");
+    }
+
+    #[test]
+    fn append_and_prepend() {
+        let mut list = LinkedList::new();
+        list.append(1);
+        list.prepend(2);
+        list.append(3);
+        list.prepend(4);
+        assert_eq!(format!("{}", list), "4 -> 2 -> 1 -> 3 -> None");
     }
 }
